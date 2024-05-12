3434fa34ed30886f60033fae29ba26ecb69a9ca3
elpandev <javoavmystery@gmail.com>
2024-05-12 15:54:36 -0500

    .

--- src/modules/student/domain/repository.ts
+++
@@ -1,6 +0,0 @@
-import type { IBaseModelRepository } from "~/elpandev/hexagonal/base/domain/repository";
-import { Student } from "./model";
-
-export interface IStudentRepository extends IBaseModelRepository<Student> {
-
-}
--- src/modules/student_question/domain/repository.ts
+++
@@ -1,6 +0,0 @@
-import type { IBaseModelRepository } from "~/elpandev/hexagonal/base/domain/repository";
-import { StudentQuestion } from "./model";
-
-export interface IStudentQuestionRepository extends IBaseModelRepository<StudentQuestion> {
-
-}
