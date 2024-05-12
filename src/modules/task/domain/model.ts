786c9e2a504d430f12a3706ed393647164524011
elpandev <javoavmystery@gmail.com>
2024-05-12 12:28:44 -0500

    .

--- src/modules/task/domain/model.ts
+++ src/modules/task/domain/model.ts
@@ -1,31 +1,37 @@
 import { Validator, required, string, min } from "@/elpandev/validator";
 import { BaseModel, type IBaseModel } from "~/elpandev/hexagonal/base/domain/model";
-import { User } from "../../user/domain/model";
 import { code_id } from "~/elpandev/utils";
-import type { Question } from "../../question/domain/model";
 
 export interface ITask extends IBaseModel {
-  id:              string
-  name:            string
-  description:     string
-  enabled:         boolean
-  start_at:        number|null
-  end_at:          number|null
-  users_count:     number
-  questions_count: number
-  points_sum:      number
+  id:                   string
+  name:                 string
+  description:          string
+  enabled:              boolean
+  start_at:             number|null
+  end_at:               number|null
+  pending_count:        number
+  started_count:        number
+  completed_count:      number
+  questions_count:      number
+  questions_points_sum: number
+  users_count:          number
+  users_points_sum:     number
 }
 
 export class Task extends BaseModel<ITask> implements ITask {
-  public id:              string      = code_id('T4gWkQn73fhuryNcHFa1vpICeKPl9BMjz2dDXsxOYGA6R5LVU8mtEwqSobZi0J')
-  public name:            string      = '';
-  public description:     string      = '';
-  public enabled:         boolean     = true;
-  public start_at:        number|null = null;
-  public end_at:          number|null = null;
-  public users_count:     number      = 0;
-  public questions_count: number      = 0;
-  public points_sum:      number      = 0;
+  public id:                   string      = code_id('T4gWkQn73fhuryNcHFa1vpICeKPl9BMjz2dDXsxOYGA6R5LVU8mtEwqSobZi0J')
+  public name:                 string      = '';
+  public description:          string      = '';
+  public enabled:              boolean     = true;
+  public start_at:             number|null = null;
+  public end_at:               number|null = null;
+  public pending_count:        number      = 0
+  public started_count:        number      = 0
+  public completed_count:      number      = 0
+  public questions_count:      number      = 0
+  public questions_points_sum: number      = 0
+  public users_count:          number      = 0
+  public users_points_sum:     number      = 0
 
   constructor(data?: Partial<ITask>) {
     super()
@@ -48,46 +54,40 @@ export class Task extends BaseModel<ITask> implements ITask {
 
   public fromPayload(data?: Partial<ITask> | undefined): this {
     if (data) {
-      if (data.id              != undefined) this.id              = data.id
-      if (data.name            != undefined) this.name            = data.name
-      if (data.description     != undefined) this.description     = data.description
-      if (data.enabled         != undefined) this.enabled         = data.enabled
-      if (data.start_at        != undefined) this.start_at        = data.start_at
-      if (data.end_at          != undefined) this.end_at          = data.end_at
-      if (data.users_count     != undefined) this.users_count     = data.users_count
-      if (data.questions_count != undefined) this.questions_count = data.questions_count
-      if (data.points_sum      != undefined) this.points_sum      = data.points_sum
+      if (data.id                   !== undefined) this.id                   = data.id
+      if (data.name                 !== undefined) this.name                 = data.name
+      if (data.description          !== undefined) this.description          = data.description
+      if (data.enabled              !== undefined) this.enabled              = data.enabled
+      if (data.start_at             !== undefined) this.start_at             = data.start_at
+      if (data.end_at               !== undefined) this.end_at               = data.end_at
+      if (data.pending_count        !== undefined) this.pending_count        = data.pending_count
+      if (data.started_count        !== undefined) this.started_count        = data.started_count
+      if (data.completed_count      !== undefined) this.completed_count      = data.completed_count
+      if (data.questions_count      !== undefined) this.questions_count      = data.questions_count
+      if (data.questions_points_sum !== undefined) this.questions_points_sum = data.questions_points_sum
+      if (data.users_count          !== undefined) this.users_count          = data.users_count
+      if (data.users_points_sum     !== undefined) this.users_points_sum     = data.users_points_sum
     }
 
     return this
   }
 
   public toPayload(): Partial<ITask> {
-    const payload: Partial<ITask> = {}
-
-    payload.id              = this.id
-    payload.name            = this.name
-    payload.description     = this.description
-    payload.enabled         = this.enabled
-    payload.start_at        = this.start_at
-    payload.end_at          = this.end_at
-    payload.users_count     = this.users_count
-    payload.questions_count = this.questions_count
-    payload.points_sum      = this.points_sum
-
-    return payload
-  }
-
-  public toStudentRelation(): Partial<ITask> {
     return {
-      id:   this.id,
-      name: this.name,
+      id:                   this.id,
+      name:                 this.name,
+      description:          this.description,
+      enabled:              this.enabled,
+      start_at:             this.start_at,
+      end_at:               this.end_at,
+      pending_count:        this.pending_count,
+      started_count:        this.started_count,
+      completed_count:      this.completed_count,
+      questions_count:      this.questions_count,
+      questions_points_sum: this.questions_points_sum,
+      users_count:          this.users_count,
+      users_points_sum:     this.users_points_sum,
     }
-  }
-
-  public calculate_points(questions: Question[]): number {
-    return questions
-      .reduce((points, question) => points += question.points, 0)
   }
 
   public get start_at_expired(): boolean {
--- src/presentation/pages/index/tasks/create.vue
+++ src/presentation/pages/index/tasks/create.vue
@@ -2,25 +2,34 @@
 import { Validator } from '@/elpandev/validator';
 import { nano_id } from "@/elpandev/utils/methods/nano_id";
 import { TaskFactory } from '~/src/modules/task/domain/factory';
-import { question_request, task_request } from '~/src/config/repositories';
+import { task_request } from '~/src/config/repositories';
 import { useSnackbar } from '~/src/presentation/states/snackbar';
-import { Question } from '~/src/modules/question/domain/model';
-import type { Task } from '~/src/modules/task/domain/model';
-import { QuestionFilter } from '~/src/modules/question/domain/filter';
-import { QuestionFactory } from '~/src/modules/question/domain/factory';
+import { Task } from '~/src/modules/task/domain/model';
+import { SelectOption } from '~/src/presentation/models/select_option';
 
 const props     = defineProps<{ task_id?: string }>()
 const title     = `${ props.task_id ? 'Editar' : 'Nueva' } Tarea`
 const validator = ref<Validator>(new Validator({ payload: {}, rules: {} }))
 const snackbar  = useSnackbar()
+const task      = ref<Task>(new Task())
 
-const store = useRequest(async () => {
+const option_enabled = computed<SelectOption<boolean>>({
+  get() { return new SelectOption({ name: task.value.enabled ? 'SI' : 'NO', value: task.value.enabled }) },
+  set(value) { task.value.enabled = value.value },
+})
+
+const options_enabled: SelectOption<boolean>[] = [
+  new SelectOption({ name: 'SI', value: true  }),
+  new SelectOption({ name: 'NO', value: false }),
+] 
+
+const { request: store, pending: store_pending } = useRequest(async () => {
   try {
-    await task_request.store(data.value!.task!)
+    await task_request.store(task.value)
 
     snackbar.value.success('La Tarea ha sido creada')
 
-    data.value!.task = new TaskFactory().generate()
+    task.value = new TaskFactory().generate()
   }
 
   catch (error) {
@@ -30,25 +39,14 @@ const store = useRequest(async () => {
   }
 })
 
-async function request_task(): Promise<Task|undefined> {
-  return props.task_id
-    ? await task_request.get(props.task_id)
+const { request: request_task } = useRequest(async () => {
+  task.value = props.task_id
+    ? await task_request.get(props.task_id) ?? new Task()
     : new TaskFactory().generate()
-}
+})
 
-async function request_questions(): Promise<Question[]> {
-  return props.task_id
-    ? await question_request.paginate(new QuestionFilter({ task_id: props.task_id }))
-    : new QuestionFactory().generate_multiple({ length: 4 })
-}
-
-const { data, pending } = await useLazyAsyncData(nano_id(), async () => {
-  const [task, questions] = await Promise.all([
-    request_task(),
-    request_questions(),
-  ])
-
-  return { task, questions }
+const { pending } = await useLazyAsyncData(nano_id(), async () => {
+  await request_task()
 })
 
 useSeoMeta({ title })
@@ -56,41 +54,37 @@ useSeoMeta({ title })
 
 <template>
   <main v-if="!pending" class="create">
-    <template v-if="data?.task && data.questions">
-      <header>
-        <h1>{{ title }}</h1>
-        <span>Puntos: {{ data.task.calculate_points(data.questions) }}</span>
-      </header>
-      <form class="form" @submit.prevent="store.request()">
-        <v-selector
-          :label="'Activo'"
-          :model-value="{ name: data!.task!.enabled ? 'SI' : 'NO', value: data!.task!.enabled }"
-          :options="[{ name: 'SI', value: true }, { name: 'NO', value: false }]"
-          @update:model-value="(value) => data!.task!.enabled = value"
-        />
+    <header>
+      <h1>{{ title }}</h1>
+      <span>Puntos: 0</span>
+    </header>
+    <form class="form" @submit.prevent="store()">
+      <v-selector
+        v-model="option_enabled"
+        :label="'Activo'"
+        :options="options_enabled"
+      />
 
-        <v-input :label="'Nombre'"       v-model="data.task.name" />
-        <v-input :label="'Descripción'"  v-model="data.task.description" :textarea="true" />
-        <v-input :label="'Calificación'" v-model="data.task.base_score" />
+      <v-input :label="'Nombre'"       v-model="task.name" />
+      <v-input :label="'Descripción'"  v-model="task.description" :textarea="true" />
 
-        <v-input
-          :label="'Fecha de Inicio'"
-          :type="'datetime-local'"
-          :model-value="data.task.start_at ? new Date(data.task.start_at).toInput() : ''"
-          @update:model-value="(value) => data!.task!.start_at = value ? new Date(value).getTime() : null"
-        />
+      <v-input
+        :label="'Fecha de Inicio'"
+        :type="'datetime-local'"
+        :model-value="task.start_at ? new Date(task.start_at).toInput() : ''"
+        @update:model-value="(value) => task.start_at = value ? new Date(value).getTime() : null"
+      />
 
-        <v-input
-          :label="'Fecha de Finalización'"
-          :type="'datetime-local'"
-          :model-value="data.task.end_at ? new Date(data.task.end_at).toInput() : ''"
-          @update:model-value="(value) => data!.task!.end_at = value ? new Date(value).getTime() : null"
-        />
+      <v-input
+        :label="'Fecha de Finalización'"
+        :type="'datetime-local'"
+        :model-value="task.end_at ? new Date(task.end_at).toInput() : ''"
+        @update:model-value="(value) => task.end_at = value ? new Date(value).getTime() : null"
+      />
 
-        <v-loader v-if="store.pending.value" />
-        <button v-else class="button outline text teal" type="submit">Guardar</button>
-      </form>
-    </template>
+      <v-loader v-if="store_pending" />
+      <button v-else class="button outline text teal" type="submit">Guardar</button>
+    </form>
   </main>
   <v-loader v-else />
 </template>
